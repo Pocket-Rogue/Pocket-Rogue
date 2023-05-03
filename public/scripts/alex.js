5bfa7f4d0c8a2c029d317751e7d17a600abe331e
adjohnson23 <johnso23@rose-hulman.edu>
2023-05-03 19:18:26 -0400

    Began javascript for creating/editing a game

---
+++ public/scripts/alex.js
@@ -0,0 +1,56 @@
+var rhit = rhit || {};
+
+/** TODO
+ * Functions to construct for edit/create game page
+ * String gameID
+ * Firebase ref
+ * getIsApproved
+ * getGameName
+ * getGameTag
+ * getGameBanner
+ * setGameBanner
+ * getGameCode
+ * setGameCode
+ * getGameIsCanvas
+ * setGameIsCanvas
+*/
+
+rhit.EditGameDataController = class {
+    constructor() {
+
+    }
+    addGame() {
+
+    }
+    updateGame() {
+
+    }
+    getIsApproved() {
+
+    }
+    getGameName() {
+
+    }
+    getGameTag() {
+
+    }
+    getGameBanner() {
+
+    }
+    setGameBanner() {
+
+    }
+    getGameCode() {
+
+    }
+    setGameCode() {
+
+    }
+    getGameIsCanvas() {
+
+    }
+    setGameIsCanvas() {
+
+    }
+
+}
