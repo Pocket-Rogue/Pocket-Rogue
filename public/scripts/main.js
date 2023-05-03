1ad480f7fe8b1836ef4c6d0a74d0ca9e8869c552
Adkins <adkinsda@rose-hulman.edu>
2023-05-03 15:49:59 -0400

    Login?

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -17,20 +17,97 @@ rhit.functionName = function () {
 	/** function body */
 };
 
-rhit.ClassName = class {
+rhit.LoginPageController = class LoginPageController {
+	constructor() {
+		document.querySelector("#rosefireButton").addEventListener("click", (event) => {
+			rhit.fbAuthManager.signIn();
+		});
+	}
+}
+
+rhit.FbAuthManager = class FbAuthManager {
+	#user = null;
+	#unsubscribe = null;
 	constructor() {
 
 	}
-
-	methodName() {
-
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this.#user = user;
+			changeListener?.();
+		})
 	}
+	signIn() {
+		Rosefire.signIn("ac52bbf0-4818-4824-a690-aa3b1c4ba045", (err, rfUser) => {
+			if (err) {
+			  console.error("Rosefire error!", err);
+			  return;
+			}
+			console.log("Rosefire success!", rfUser);
+			
+
+			firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
+				const { errorCode, errorMessage } = error;
+				if(errorCode === "auth/invalid-custom-token") {
+					alert("The token you provided is not useful");
+				} else {
+					console.error("Custom auth error", errorCode, errorMessage);
+				}
+			})
+			// TODO: Use the rfUser.token with your server.
+		  });
+		  
+	}
+	signOut() {
+		firebase.auth().signOut().catch((error) => {
+			// An error happened.
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			console.log("Signout user error", errorCode, errorMessage);
+		});
+	}
+	get isSignedIn() {
+		return this.#user != null;
+	}
+	get uid() {
+		// Null chaining!
+		return this.#user?.uid;
+	}
+}
+
+rhit.checkForRedirects = function() {
+	if (document.querySelector("#loginPage")) {
+		if(rhit.fbAuthManager.isSignedIn) {
+			window.location.href = "/list.html";
+		}
+	} else {
+		if(!rhit.fbAuthManager.isSignedIn) {
+			window.location.href = "/";
+		}
+	}
+}
+
+rhit.initializePage = function() {
+	const pageUrl = new URL(window.location.href); 
+		if (document.querySelector("#loginPage")) {
+			console.log("Login page");
+			new rhit.LoginPageController();
+		}
 }
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	rhit.fbAuthManager = new rhit.FbAuthManager();
+	rhit.fbAuthManager.beginListening(() => {
+		console.log("auth change callback fired. TODO: check for redirect and init the page");
+		console.log("isSignedIn = ", rhit.fbAuthManager.isSignedIn);
+		// Check for redirects
+		rhit.checkForRedirects();
+		// Initialize page
+		rhit.initializePage();
+	});
 };
 
 rhit.main();
