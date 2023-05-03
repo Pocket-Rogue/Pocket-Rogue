69919f6e26af1d3f199c0ee087f2f1c2a5660d52
Adkins <adkinsda@rose-hulman.edu>
2023-05-03 16:53:42 -0400

    Color!

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -25,6 +25,32 @@ rhit.LoginPageController = class LoginPageController {
 	}
 }
 
+function cyrb128(str) {
+    let h1 = 1779033703, h2 = 3144134277,
+        h3 = 1013904242, h4 = 2773480762;
+    for (let i = 0, k; i < str.length; i++) {
+        k = str.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    return [(h1^h2^h3^h4)>>>0, (h2^h1)>>>0, (h3^h1)>>>0, (h4^h1)>>>0];
+}
+
+function mulberry32(a) {
+    return function() {
+      var t = a += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+}
+
 rhit.FbAuthManager = class FbAuthManager {
 	#user = null;
 	#unsubscribe = null;
@@ -35,6 +61,10 @@ rhit.FbAuthManager = class FbAuthManager {
 		firebase.auth().onAuthStateChanged((user) => {
 			this.#user = user;
 			$("#profileImage").text(user.uid[0].toUpperCase())
+			var seed = cyrb128(user.uid);
+			
+			$("#profileImage").css("background-color", `#${Math.floor(0xFFFFFF * mulberry32(seed[0])()).toString(16).padStart(6,"0")}`)
+			console.log(`#${mulberry32(seed[0])().toString(16).padStart(6,"0")}`);
 			changeListener?.();
 		})
 	}
