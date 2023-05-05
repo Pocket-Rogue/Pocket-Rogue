9b4be44de8d53a4ed2ba7e36dcf542b28bf87ca1
adjohnson23 <johnso23@rose-hulman.edu>
2023-05-05 16:03:38 -0400

    Integrated Create/Edit Game page to main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,3 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
 rhit.FB_COLLECTION_GAMES = "Games";
@@ -17,6 +8,10 @@ rhit.FB_KEY_STARS = "totalStars";
 rhit.FB_KEY_NUMRATINGS = "totalRatings";
 rhit.FB_KEY_DESCRIPTION = "description";
 rhit.FB_KEY_BANNERCOLOR = "BannerColor";
+rhit.FB_KEY_APPROVED = "approved";
+rhit.FB_KEY_CODE = "code";
+rhit.FB_KEY_ICON = "icon";
+rhit.FB_KEY_ISCANVAS = "isCanvas";
 
 rhit.FB_COLLECTION_USERS = "Users";
 rhit.FB_COLLECTION_PLAYEDGAMES = "playedGames";
@@ -24,6 +19,8 @@ rhit.FB_KEY_FAVORITE = "favorited";
 rhit.FB_KEY_RATED = "isRated";
 rhit.fbSingleGameManager = null;
 rhit.fbAuthManager = null;
+rhit.ECGameManager = null;
+rhit.GameId = null;
 
 rhit.GamePageController = class {
 	constructor() {
@@ -214,6 +211,385 @@ rhit.FbAuthManager = class FbAuthManager {
 	}
 }
 
+rhit.EditGameDataController = class {
+    constructor(gameId) {
+        console.log("Added EditGameDataController.");
+
+        document.querySelector('#imageForm').addEventListener('submit', this.handleSubmit);
+        document.querySelector('#logoForm').addEventListener('submit', this.handleSubmit);
+        document.querySelector('#gameForm').addEventListener('submit', this.handleSubmit);
+        document.querySelector("#imageFile").addEventListener('change', (event) => {
+            console.log("Upload a main image!");
+            rhit.ECGameManager.loadMainImage(event);
+        });
+        document.querySelector("#logoFile").addEventListener('change', (event) => {
+            console.log("Upload a logo!");
+            rhit.ECGameManager.loadLogoImage(event);
+        });
+        document.querySelector("#gameFile").addEventListener('change', (event) => {
+            console.log("Upload a javascript file!");
+            rhit.ECGameManager.loadGameFile(event);
+        })
+        document.querySelector("#colorButton").addEventListener('change', (event) => {
+            console.log("Change the color!");
+            rhit.ECGameManager.changeColor();
+        });
+        document.querySelector("#createButton").addEventListener('click', (event) => {
+            console.log("Create a new game!");
+            rhit.ECGameManager.addGame();
+        });
+        document.querySelector("#viewButton").addEventListener('click', (event) => {
+            console.log("View the game!");
+            window.location.href = `game.html?id=${rhit.gameId}`;
+        });
+        document.querySelector("#saveButton").addEventListener('click', (event) => {
+            console.log("Save the game!");
+            rhit.ECGameManager.saveGame();
+        });
+        document.querySelector("#deleteButton").addEventListener('click', (event) => {
+            console.log("Delete the game!");
+            rhit.ECGameManager.deleteGame()
+            .then(() => {
+                console.log("Game successfully deleted!");
+                window.location.href = "index.html";
+            }).catch((error) => {
+                console.error("Error deleting game: ", error);
+            });
+        });
+
+        if (gameId) {
+            rhit.ECGameManager.beginListening(this.showExistingGame.bind(this));
+        }
+    }
+
+    // Handle upload event here. Credit: https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
+    handleSubmit(event) {
+        const form = event.currentTarget;
+        const url = new URL(form.action);
+        const formData = new FormData(form);
+        const searchParams = new URLSearchParams(formData);
+
+        if(form.method.toLowerCase() == 'post') {
+            if(form.enctype === 'multipart/form-data') {
+                fetchOptions.body = formData;
+            } else {
+                fetchOptions.body = searchParams;
+            }
+        } else {
+            url.search = searchParams;
+        }
+
+        fetch(url, {
+            method: form.method,
+            body: formData,
+        });
+
+        // Any JS that could fail goes here
+        event.preventDefault();
+
+    }
+
+    // Load in existing game resources
+    showExistingGame() {
+        console.log("Attempting to show an existing game");
+        document.querySelector("#uploadImage").src = rhit.ECGameManager.mainImage;
+        document.querySelector("#uploadLogo").src = rhit.ECGameManager.logoImage;
+        document.querySelector("#gameCaption").style.backgroundColor = rhit.ECGameManager.captionColor;
+        document.querySelector("#colorButton").value = rhit.ECGameManager.captionColor;
+        document.querySelector("#gameScript").value = rhit.ECGameManager.jsGameString;
+        document.querySelector("#inputTitle").value = rhit.ECGameManager.title;
+        document.querySelector("#inputAuthor").value = rhit.ECGameManager.author;
+        document.querySelector("#inputDescription").value = rhit.ECGameManager.description;
+    }
+}
+
+rhit.editGameDataManager = class {
+    constructor(gameId) {
+        this._documentSnapshot = {};
+        this._unsubscribe = null;
+        console.log("Created editGameDataManager.");
+        rhit.gameId = gameId;
+        if (!gameId) {
+            console.log("We're creating a new game!");
+            this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_GAMES);
+        } else {
+            this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_GAMES).doc(gameId);
+            console.log(`Listing to ${this._ref.path}`);
+            this._showEditButtons();
+        }
+    }
+    beginListening(changeListener) {
+        let gameExists = false;
+		this._unsubscribe = this._ref.onSnapshot((doc) => {
+			if (doc.exists) {
+                gameExists = true;
+				console.log("Document data:", doc.data());
+				this._documentSnapshot = doc;
+				changeListener();
+			} else {
+				// doc.data() will be undefined in this case
+				console.log("No such document!");
+                if (!gameExists) {
+                    window.location.href = "404.html";
+                }
+			}
+		});
+	}
+	stopListening() {
+		this._unsubscribe();
+	}
+
+    // If we're in edit mode, hide the publish button and show the other buttons
+    _showEditButtons() {
+        document.querySelector("#gameLabel").innerHTML = "Overwrite existing js file (optional)";
+        document.querySelector("#createButton").style.visibility = "hidden";
+        document.querySelector("#viewButton").style.visibility = "visible";
+        document.querySelector("#saveButton").style.visibility = "visible";
+        document.querySelector("#deleteButton").style.visibility = "visible";
+        console.log("Buttons should be toggled");
+    }
+    // Handle image upload display here
+    loadMainImage(event) {
+        var image = document.getElementById('uploadImage');
+        //image.src = URL.createObjectURL(event.target.files[0]);
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = function() {
+            const base64 = reader.result;
+            image.src = base64;
+        };
+
+        image.src = reader.readAsDataURL(file);
+    }
+    loadLogoImage(event) {
+        var image = document.getElementById('uploadLogo');
+        //image.src = URL.createObjectURL(event.target.files[0]);
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = function() {
+            const base64 = reader.result;
+            image.src = base64;
+        };
+
+        reader.readAsDataURL(file);
+    }
+    loadGameFile(event) {
+        var game = document.getElementById('gameScript');
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = function() {
+            const base64 = reader.result;
+            console.log(base64);
+            game.value = base64;
+        }
+
+        reader.readAsDataURL(file);
+    }
+
+    // Handle color change here
+    changeColor() {
+        var caption = document.getElementById('gameCaption');
+        var chosenColor = document.getElementById('colorButton');
+        var title = document.getElementById('inputTitle');
+        var author = document.getElementById('inputAuthor');
+        let color = chosenColor.value;
+        // Calculate the luma, used to determine if text color needs to change
+        // LUMA CALCULATION CREDIT: https://stackoverflow.com/questions/12043187/how-to-check-if-hex-color-is-too-black
+        color = color.substring(1);
+        var rgb = parseInt(color, 16);
+        var r = (rgb >> 16) & 0xff;  // extract red
+        var g = (rgb >>  8) & 0xff;  // extract green
+        var b = (rgb >>  0) & 0xff;  // extract blue
+
+        var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+        if (luma < 60) {
+            // Change text color to be light, otherwise its dark
+            title.style.color = "#FFFFFF";
+            author.style.color = "#FFFFFF";
+        } else {
+            title.style.color = "#111111";
+            author.style.color = "#111111";
+        }
+        caption.style.backgroundColor = chosenColor.value;
+    }
+
+    // Handle adding a new game here. Occurs when "publish" is pressed.
+    /**
+     * Obtain the mainImage src (banner), logoImage src (Icon), style color of the caption (BannerColor),
+     * the js file string (Code), title (Title), author (AuthorName), and the description (Description).
+     * Check that all of these entries are valid, aka they aren't empty.
+     * If one of them is empty, an alert needs to be given that that field needs to be filled.
+     * Note: Description is allowed to be empty.
+     * Otherwise, proceed to create a new firestore Games Item. 
+     */
+    addGame() {
+        // Fetch game values
+        console.log("Attempting to publish game...");
+        var mainImage = document.querySelector("#uploadImage").src;
+        var logoImage = document.querySelector("#uploadLogo").src;
+        var captionColor = document.querySelector("#colorButton").value;
+        var jsGameString = document.querySelector("#gameScript").value;
+        var title = document.querySelector("#inputTitle").value;
+        var author = document.querySelector("#inputAuthor").value;
+        var description = document.querySelector("#inputDescription").value; 
+        console.log(`Game values found. Here are the following parameters:\nmain image: ${mainImage}\nlogo image: ${logoImage}
+        \ncaption color: ${captionColor}\njsGameString: ${jsGameString}\ntitle: ${title}\nauthor: ${author}\ndescription: ${description} `);
+
+        // Inspect game values: Build alert string
+        let alertString = "You are unable to publish your game: ";
+        let invalidPublish = false;
+        if (mainImage.includes("images/empty_image.jpg")) {
+            invalidPublish = true;
+            alertString = alertString.concat("\nPlease upload a banner image.");
+        }
+        if (logoImage.includes("images/empty_image.jpg")) {
+            invalidPublish = true;
+            alertString = alertString.concat("\nPlease upload a logo image.");
+        }
+        /*if (jsGameString.length == 0) {
+            invalidPublish = true;
+            alertString = alertString.concat("\nPlease upload a javascript game file.");
+        }*/
+        if (title.length == 0) {
+            invalidPublish = true;
+            alertString = alertString.concat("\nPlease add a title.");
+        }
+        if (author.length == 0) {
+            invalidPublish = true;
+            alertString = alertString.concat("\nPlease add an author.");
+        }
+
+        // Alert user a message if publish isn't done
+        if (invalidPublish) {
+            alert(alertString);
+            return;
+        }
+        console.log("Passed validity check. Proceeding to publish the game...");
+        console.log("Adding game ", this._ref);
+
+        // Add a new entry to the Games collection in firestore.
+        this._ref.add({
+            [rhit.FB_KEY_BANNERCOLOR]: captionColor,
+            [rhit.FB_KEY_APPROVED]: false,
+            [rhit.FB_KEY_IMAGE]: mainImage,
+            [rhit.FB_KEY_CODE]: jsGameString,
+            [rhit.FB_KEY_DESCRIPTION]: description,
+            [rhit.FB_KEY_AUTHOR]: author,
+            [rhit.FB_KEY_ICON]: logoImage,
+            [rhit.FB_KEY_ISCANVAS]: false,
+            [rhit.FB_KEY_TITLE]: title,
+            [rhit.FB_KEY_NUMRATINGS]: 0,
+            [rhit.FB_KEY_STARS]: 0,
+        })
+        .then((docRef) => {
+            console.log("Game published with ID: ", docRef.id);
+            window.location.href = "index.html";
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+
+        return;
+    }
+
+    saveGame() {
+        // Fetch game values
+        console.log("Saving changes to the game...");
+        var mainImage = document.querySelector("#uploadImage").src;
+        var logoImage = document.querySelector("#uploadLogo").src;
+        var captionColor = document.querySelector("#colorButton").value;
+        var jsGameString = document.querySelector("#gameScript").value;
+        var title = document.querySelector("#inputTitle").value;
+        var author = document.querySelector("#inputAuthor").value;
+        var description = document.querySelector("#inputDescription").value; 
+        console.log(`Game values found. Here are the following parameters:\nmain image: ${mainImage}\nlogo image: ${logoImage}
+        \ncaption color: ${captionColor}\njsGameString: ${jsGameString}\ntitle: ${title}\nauthor: ${author}\ndescription: ${description} `);
+
+        // Inspect game values: Build alert string
+        let alertString = "You are unable to your changes: ";
+        let invalidPublish = false;
+        if (mainImage.includes("images/empty_image.jpg")) {
+            invalidPublish = true;
+            alertString = alertString.concat("\nPlease upload a banner image.");
+        }
+        if (logoImage.includes("images/empty_image.jpg")) {
+            invalidPublish = true;
+            alertString = alertString.concat("\nPlease upload a logo image.");
+        }
+        /*if (jsGameString.length == 0) {
+            invalidPublish = true;
+            alertString = alertString.concat("\nPlease upload a javascript game file.");
+        }*/
+        if (title.length == 0) {
+            invalidPublish = true;
+            alertString = alertString.concat("\nPlease add a title.");
+        }
+        if (author.length == 0) {
+            invalidPublish = true;
+            alertString = alertString.concat("\nPlease add an author.");
+        }
+
+        // Alert user a message if save isn't done
+        if (invalidPublish) {
+            alert(alertString);
+            return;
+        }
+        console.log("Passed validity check. Proceeding to save changes...");
+
+        console.log("Updating the game ", this._ref);
+        // Update the existing entry to the Games collection in firestore.
+        this._ref.update({
+            [rhit.FB_KEY_BANNERCOLOR]: captionColor,
+            [rhit.FB_KEY_IMAGE]: mainImage,
+            [rhit.FB_KEY_CODE]: jsGameString,
+            [rhit.FB_KEY_DESCRIPTION]: description,
+            [rhit.FB_KEY_AUTHOR]: author,
+            [rhit.FB_KEY_ICON]: logoImage,
+            [rhit.FB_KEY_TITLE]: title,
+        })
+        .then(() => {
+            console.log("Changes saved!");
+            window.location.href = "index.html";
+        })
+        .catch((error) => {
+            console.error("Error saving changes: ", error);
+        });
+
+        return;
+    }
+
+    deleteGame() {
+        console.log("Deleting the following game: ", this._ref);
+        return this._ref.delete();
+    }
+
+    get mainImage() {
+        return this._documentSnapshot.get(rhit.FB_KEY_IMAGE);
+    }
+    get logoImage() {
+        return this._documentSnapshot.get(rhit.FB_KEY_ICON);
+    }
+    get captionColor() {
+        return this._documentSnapshot.get(rhit.FB_KEY_BANNERCOLOR);
+    }
+    get jsGameString() {
+        return this._documentSnapshot.get(rhit.FB_KEY_CODE);
+    }
+    get title() {
+        return this._documentSnapshot.get(rhit.FB_KEY_TITLE);
+    }
+    get author() {
+        return this._documentSnapshot.get(rhit.FB_KEY_AUTHOR);
+    }
+    get description() {
+        return this._documentSnapshot.get(rhit.FB_KEY_DESCRIPTION);
+    }
+
+}
+
 rhit.checkForRedirects = function() {
 	if (document.querySelector("#loginPage")) {
 		if(rhit.fbAuthManager.isSignedIn) {
@@ -242,6 +618,24 @@ rhit.initializePage = () => {
 	if (document.querySelector("#loginPage")) {
 		console.log("Login page");
 		new rhit.LoginPageController();
+	}
+	if(document.querySelector("#editPage")) {
+		console.log("You are on the edit/create game page.");
+
+        // Determine if there is a game id being passed (that means we're editing a game)
+        const queryString = window.location.search;
+		console.log(queryString);
+		const urlParams = new URLSearchParams(queryString);
+		const gameId = urlParams.get("id");
+
+		if (!gameId) {
+            console.log("Creating a new game");
+		} else {
+            console.log("Editing an existing game");
+        }
+
+        rhit.ECGameManager = new rhit.editGameDataManager(gameId);
+		new rhit.EditGameDataController(gameId);
 	}
 };
 
