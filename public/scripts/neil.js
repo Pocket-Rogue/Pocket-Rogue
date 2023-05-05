b4db11dbe95ba377646555ff96a86c12581af7c1
Newake <nwagneroke@gmail.com>
2023-05-05 15:25:58 -0400

    game page

--- public/scripts/neil.js
+++ public/scripts/neil.js
@@ -1,13 +1,16 @@
 var rhit = rhit || {};
 
 rhit.FB_COLLECTION_GAMES = "Games";
-rhit.FB_KEY_IMAGE = "image";
+rhit.FB_KEY_IMAGE = "banner";
 rhit.FB_KEY_TITLE = "title";
 rhit.FB_KEY_AUTHOR = "developer";
 rhit.FB_KEY_STARS = "totalStars";
 rhit.FB_KEY_NUMRATINGS = "totalRatings";
 rhit.FB_KEY_DESCRIPTION = "description";
+rhit.FB_KEY_BANNERCOLOR = "BannerColor";
+
 rhit.FB_COLLECTION_USERS = "Users";
+rhit.FB_COLLECTION_PLAYEDGAMES = "playedGames";
 rhit.FB_KEY_FAVORITE = "favorited";
 rhit.FB_KEY_RATED = "isRated";
 rhit.fbSingleGameManager = null;
@@ -15,23 +18,24 @@ rhit.fbAuthManager = null;
 
 rhit.GamePageController = class {
 	constructor() {
-		// document.querySelector("#menuSignOut").onclick = (event) => {
-		// 	rhit.fbAuthManager.signOut();
-		// }
+		document.querySelector("#review").onclick = (event) => {
+			
+		}
 		rhit.fbSingleGameManager.beginListening(this.updateView.bind(this));
 	}
 	updateView() {
-		document.querySelector("#mainImage").innerHTML = rhit.fbSingleGameManager.image;
+		document.querySelector("#mainImage").src = rhit.fbSingleGameManager.image;
+		document.querySelector("#autherTitleBox").style.backgroundColor = rhit.fbSingleGameManager.color;
 		document.querySelector("#gameTitle").innerHTML = rhit.fbSingleGameManager.title;
 		document.querySelector("#author").innerHTML = rhit.fbSingleGameManager.author;
 		document.querySelector("#description").innerHTML = rhit.fbSingleGameManager.description;
 
-		const stars = Math.round(2 * rhit.fbSingleGameManager.stars / rhit.fbSingleGameManager.numRatings) / 2;
+		const stars = (rhit.fbSingleGameManager.stars == 0 ? 0 : Math.round(2 * rhit.fbSingleGameManager.stars / rhit.fbSingleGameManager.numRatings) / 2);
 		const intStars = parseInt(stars);
 		document.querySelector("#reveiws").innerHTML = "<i class='material-icons'>star</i>".repeat(intStars) +
 			(intStars != stars ? "<i class='material-icons'>star_half</i>" : "") +
 			"<i class='material-icons'>star_border</i>".repeat(5 - stars) +
-			"&nbsp;" + rhit.fbSingleGameManager.numRatings + "reviews";
+			"&nbsp;" + rhit.fbSingleGameManager.numRatings + " reviews";
 		document.querySelector("#starButton").innerHTML = (rhit.fbSingleGameManager.favorited ? "<i class='material-icons'>star</i>" : "<i class='material-icons'>star_border</i>")
 		document.querySelector("#review").innerHTML = (rhit.fbSingleGameManager.rated ? "Edit Star Rating" : "Add Star Rating")
 		// if (rhit.fbSingleGameManager.author == rhit.fbAuthManager.uid) {
@@ -48,7 +52,7 @@ rhit.FbSingleGameManager = class {
 		this._gameUnsubscribe = null;
 		this._userGameUnsubscribe = null;
 		this._gameRef = firebase.firestore().collection(rhit.FB_COLLECTION_GAMES).doc(gameId);
-		this._userGameRef = firebase.firestore().collection(rhit.FB_COLLECTION_USERS).doc(rhit.fbAuthManager.uid).collection("playedGames").doc(gameId);
+		this._userGameRef = firebase.firestore().collection(rhit.FB_COLLECTION_USERS).doc(rhit.fbAuthManager.uid).collection(rhit.FB_COLLECTION_PLAYEDGAMES).doc(gameId);
 		// this._userGameRef = firebase.firestore().collection(rhit.FB_COLLECTION_USERS).doc("adkinsda").collection("playedGames").doc(gameId);
 	}
 	beginListening(changeListener) {
@@ -72,6 +76,9 @@ rhit.FbSingleGameManager = class {
 
 	get image() {
 		return this._gameDocumentSnapshot.get(rhit.FB_KEY_IMAGE);
+	}
+	get color() {
+		return this._gameDocumentSnapshot.get(rhit.FB_KEY_BANNERCOLOR);
 	}
 	get title() {
 		return this._gameDocumentSnapshot.get(rhit.FB_KEY_TITLE);
@@ -108,46 +115,46 @@ rhit.FbSingleGameManager = class {
 	}
 }
 
-rhit.FbAuthManager = class {
-	constructor() {
-		this._user = null;
-	}
-	beginListening(changeListener) {
-		firebase.auth().onAuthStateChanged((user) => {
-			this._user = user;
-			changeListener();
-		});
-	}
-	signIn() {
-		console.log("TODO: Sign in using Rosefire");
-		Rosefire.signIn("84d5e5a9-7ee8-4172-b06f-a70e5d8eb874", (err, rfUser) => {
-			if (err) {
-				console.log("Rosefire error!", err);
-				return;
-			}
-			console.log("Rosefire success!", rfUser);
+// rhit.FbAuthManager = class {
+// 	constructor() {
+// 		this._user = null;
+// 	}
+// 	beginListening(changeListener) {
+// 		firebase.auth().onAuthStateChanged((user) => {
+// 			this._user = user;
+// 			changeListener();
+// 		});
+// 	}
+// 	signIn() {
+// 		console.log("TODO: Sign in using Rosefire");
+// 		Rosefire.signIn("84d5e5a9-7ee8-4172-b06f-a70e5d8eb874", (err, rfUser) => {
+// 			if (err) {
+// 				console.log("Rosefire error!", err);
+// 				return;
+// 			}
+// 			console.log("Rosefire success!", rfUser);
 
-			firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
-				const errorCode = error.code;
-				const errorMessage = error.message;
-				if (errorCode === "auth/invalid-custom-token") {
-					alert("The token you provided is not valid.")
-				} else {
-					console.log("Custom auth error", errorCode, errorMessage);
-				}
-			});
-		});
-	}
-	signOut() {
-		firebase.auth().signOut();
-	}
-	get uid() {
-		return this._user.uid;
-	}
-	get isSignedIn() {
-		return !!this._user;
-	}
-}
+// 			firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
+// 				const errorCode = error.code;
+// 				const errorMessage = error.message;
+// 				if (errorCode === "auth/invalid-custom-token") {
+// 					alert("The token you provided is not valid.")
+// 				} else {
+// 					console.log("Custom auth error", errorCode, errorMessage);
+// 				}
+// 			});
+// 		});
+// 	}
+// 	signOut() {
+// 		firebase.auth().signOut();
+// 	}
+// 	get uid() {
+// 		return this._user.uid;
+// 	}
+// 	get isSignedIn() {
+// 		return !!this._user;
+// 	}
+// }
 
 rhit.initializePage = () => {
 	const urlParams = new URLSearchParams(window.location.search);
